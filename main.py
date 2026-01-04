b0edf177797771a631c8c8345724ce8c0de9c0ce
tda88 <teemudaaltonen@gmail.com>
2026-01-04 23:16:02 +0200

    6: Made main.py to import functions from stats.py. main.py prints the word count and used characters from a file.

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 # Boot.dev course project: Bookbot
-from stats import get_words_count
+from stats import get_book_text, get_words_count, get_chars_count
 
 def main():
+    chars = {}
+    book = ""
+
     with open("./books/frankenstein.txt") as f:
-        print(f"Found {get_words_count(f)} total words")
+        book = get_book_text(f)
+    
+    print(f"Found {get_words_count(book)} total words")
+    print("Characters used:")
+    chars = get_chars_count(book)
+    print(chars)
+
+    
+                  
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,18 @@
 def get_book_text(filepath):
     return filepath.read()
 
-def get_words_count(filepath):
-    word_list = get_book_text(filepath).split() 
+def get_words_count(book):
+    word_list = book.split() 
     return len(word_list)
+
+def get_chars_count(book):
+    book = book.lower()
+    chars = {}
+
+    for c in book:
+        if c not in chars:
+            chars[c] = 1
+        else:
+            chars[c] += 1
+
+    return chars
